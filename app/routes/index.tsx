16e8f765027dc2cefdbac29288ba5c8c30f06c86
Bob Owen <bob@bobowen.org>
2022-07-09 14:29:10 -0400

    Added Prisma and minimal functionality

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,41 @@
+import { ActionFunction, json, LoaderFunction } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { feedNow, getLastFedInfo } from "~/utils/feedingHistory.server";
+import { requireUserId } from "~/utils/session.server";
+
+interface LoaderData {
+  fedTodayCount: number;
+  lastFed: string;
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
+
+  const lastFedInfo = await getLastFedInfo(userId);
+
+  return json(lastFedInfo);
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
+  await feedNow(userId);
+  return null;
+};
+
 export default function Index() {
+  const { fedTodayCount, lastFed } = useLoaderData<LoaderData>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>🐶 Fed the dog</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        <li>Fed: {fedTodayCount}</li>
+        <li>Last: {lastFed}</li>
       </ul>
+      <Form method="post">
+        <input type="hidden" name="_method" value="feed" />
+        <button type="submit">Feed now</button>
+      </Form>
     </div>
   );
 }
